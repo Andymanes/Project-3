c294a6cf04f2a867d3c9b20770056c6eef771a2b
Andymanes <101294232+Andymanes@users.noreply.github.com>
2022-05-17 12:03:19 -0400

    struggling with api

--- src/App.js
+++ src/App.js
@@ -6,34 +6,56 @@ import GamesList from './pages/GamesList';
 
 
 
+// function App() {
+//   const [games, setGames] = useState([])
+  
+
+  
+//   async function getGames() {
+//     try {
+//     let response = await fetch('https://www.giantbomb.com/api/games/?format=json&api_key=4475e47564e8bd765fc769b0d2298b654984a973&filter=platforms:9')
+//   } catch(err) {
+//       return (err)
+//   }}
+
+//   useEffect(() => {
+//     getGames()
+//   }, [])
+
+//   if (games === null) {
+//     return <h3>Loading</h3>
+//   }
+
+//   return (
+//     <Routes>
+//       <Route exact path='/' element={<GamesList games={games} />} />
+//     </Routes>
+//   )
+// }
+
+
 function App() {
-  const [games, setGames] = useState([])
-  const config = {
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    mode:'cors'
 
-  }
-  function getGames() {
-    fetch('https://www.giantbomb.com/api/games/?format=json&api_key=4475e47564e8bd765fc769b0d2298b654984a973&filter=platforms:9', config)
-    .then((res) => res.json())
-    .then((res) => setGames(res.results))
-  }
+ 
+  let [dogs, setDogs] = useState(null)
 
+    
   useEffect(() => {
-    getGames()
-  }, [])
-
-  if (games === null) {
-    return <h3>Loading</h3>
-  }
-
+    fetch("https://dog.ceo/api/breeds/list/all")
+    .then(response => response.json())
+    .then(data => setDogs(data.message))
+  },[])
+  console.log({dogs})
   return (
-    <Routes>
-      <Route exact path='/' element={<GamesList games={games} />} />
-    </Routes>
-  )
+    <div className="App">
+        {Object.keys(dogs).map((eachDog, idx) => {
+          <div key={idx}>
+            <h1>{dogs.message}</h1>
+          </div>
+        })}
+    </div>
+  );
 }
+
 
 export default App;
