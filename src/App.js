3a6bfd585ff74a2bafc193d61a0938568189e28b
Andymanes <101294232+Andymanes@users.noreply.github.com>
2022-05-17 22:22:03 -0400

    rendering api on page

--- src/App.js
+++ src/App.js
@@ -1,61 +1,81 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom'
+// import './App.css';
+// import { Routes, Route } from 'react-router-dom'
 import React from 'react'
 import { useState, useEffect } from 'react'
-import GamesList from './pages/GamesList';
+// import GamesList from './pages/GamesList';
 
-
-
-// function App() {
-//   const [games, setGames] = useState([])
-  
-
-  
-//   async function getGames() {
-//     try {
-//     let response = await fetch('https://www.giantbomb.com/api/games/?format=json&api_key=4475e47564e8bd765fc769b0d2298b654984a973&filter=platforms:9')
-//   } catch(err) {
-//       return (err)
-//   }}
-
-//   useEffect(() => {
-//     getGames()
-//   }, [])
-
-//   if (games === null) {
-//     return <h3>Loading</h3>
-//   }
-
-//   return (
-//     <Routes>
-//       <Route exact path='/' element={<GamesList games={games} />} />
-//     </Routes>
-//   )
-// }
 
 
 function App() {
 
- 
-  let [dogs, setDogs] = useState(null)
+  let [books, getBooks] = useState([])
 
-    
   useEffect(() => {
-    fetch("https://dog.ceo/api/breeds/list/all")
-    .then(response => response.json())
-    .then(data => setDogs(data.message))
+    console.log('useEffect happned')
+    fetch('https://www.googleapis.com/books/v1/volumes?q=subject:fantasy&key=AIzaSyBTjTd2shcZdt0Zac3pxR_RV72Jj7QxWqw')
+    .then(response => {
+      console.log('Here is response:', response)
+      return response.json()
+    })
+    .then(data => {
+      console.log('Here is data:', data)
+      getBooks(data.items)
+    })
   },[])
-  console.log({dogs})
-  return (
-    <div className="App">
-        {Object.keys(dogs).map((eachDog, idx) => {
+console.log({books})
+  return(
+    <div className='App'>
+
+        {books ? books.map((book, idx) => (
           <div key={idx}>
-            <h1>{dogs.message}</h1>
+            <h2>{book.volumeInfo?.title}</h2>
+
           </div>
-        })}
+            
+
+        )) : <p>Loading...</p>}
+        
+
     </div>
-  );
+  )
 }
 
+
+
+// function App() {
+
+ 
+//   let [dogs, setDogs] = useState(null)
+
+    
+//   useEffect(() => {
+//     console.log("UseEffect happened!")
+//     fetch("https://dog.ceo/api/breeds/list/all")
+//     .then(response => {
+//       console.log("Here is response: ", response)
+//       return response.json()
+//     })
+//     .then(data => {
+//       console.log("Here is data: ", data)
+//       setDogs(data.message)
+//     })
+//   },[])
+
+//   return (
+//     <div className="App">
+//       {
+//         dogs
+//         ?
+//         Object.keys(dogs).map((eachDog, idx) => {
+//             return(
+//               <p key={idx}>{eachDog}</p>
+//             )
+//         })
+//         :
+//         <p>Loading...</p>
+//       }
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/pages/BookList.js
@@ -0,0 +1,20 @@
+// import { Link } from 'react-router-dom'
+
+// function GamesList(props) {
+//     console.log(props.games)
+
+//     return (
+//         <>
+//             {props.games ? props.games.map((game, idx) => (
+//                 <div key={idx}>
+                    
+//                         <h2>{game.name}</h2>
+                     
+//                 </div>
+//             )) : <h3>LOADING! PLEASE WAIT!</h3>}
+//         </>
+//     )
+// }
+
+
+// export default GamesList
